be5359487b294c08dbea12e04b8094d4d77f2e16
srvenient <srvenient@gmail.com>
2026-01-22 22:38:27 -0500

    feat(catalog): add async findAll method to CategoryAggregateRootRepository

--- backend/core/src/main/java/team/emptyte/katharsis/catalog/domain/category/domain/CategoryAggregateRootRepository.java
+++ backend/core/src/main/java/team/emptyte/katharsis/catalog/domain/category/domain/CategoryAggregateRootRepository.java
@@ -1,9 +1,12 @@
 package team.emptyte.katharsis.catalog.domain.category.domain;
 
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 public interface CategoryAggregateRootRepository {
   CompletableFuture<CategoryAggregateRoot> findAsync(final Long id);
+
+  CompletableFuture<Collection<CategoryAggregateRoot>> findAllAsync();
 
   CompletableFuture<Void> saveAsync(final CategoryAggregateRoot categoryAggregateRoot);
 }
