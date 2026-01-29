ce8b78d02b197d92ff24557b16123b2603f9f4bd
srvenient <srvenient@gmail.com>
2026-01-29 15:24:55 -0500

    feat(branch): add BranchController with endpoints for branch management

---
+++ backend/app/src/main/java/team/emptyte/katharsis/branch/infrastructure/api/BranchController.java
@@ -0,0 +1,32 @@
+package team.emptyte.katharsis.branch.infrastructure.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import team.emptyte.katharsis.branch.application.BranchService;
+import team.emptyte.katharsis.branch.application.contract.command.BranchCommand;
+import team.emptyte.katharsis.branch.application.contract.view.BranchView;
+import team.emptyte.katharsis.catalog.category.application.CategoryService;
+import team.emptyte.katharsis.catalog.category.application.contract.command.CategoryCommand;
+import team.emptyte.katharsis.catalog.category.application.contract.view.CategoryView;
+
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+@Tag(name = "Category API")
+public final class BranchController {
+  private final BranchService branchService;
+
+  @GetMapping("/{id}")
+  public CompletableFuture<BranchView> get(final @PathVariable Long id) {
+    return this.branchService.findById(id);
+  }
+
+  @PostMapping
+  public CompletableFuture<Void> create(final @RequestBody BranchCommand branchCommand) {
+    return this.branchService.create(branchCommand);
+  }
+}
