c2634d7dfa73ec29a53d9a945acb6fd307f2e60c
srvenient <srvenient@gmail.com>
2026-01-28 23:04:44 -0500

    feat(frontend): enhance sidebar with admin roles and branch details

--- frontend/src/app/(protected)/dashboard/layout.tsx
+++ frontend/src/app/(protected)/dashboard/layout.tsx
@@ -13,8 +13,19 @@ export default function DashboardLayout({
 }) {
   return (
     <section className="flex h-screen w-full overflow-hidden">
-      <aside className="h-full shrink-0 p-4">
-        <Sidebar />
+      <aside className="h-full w-52 shrink-0">
+        <Sidebar
+          role="admin"
+          user={{
+            name: "Nelson R.",
+            email: "nrodriguezr@gmail.com",
+            avatarUrl: "/images/png/avatar.png"
+          }}
+          currentBranch={{
+            name: "Mosquera Main",
+            type: "Distribution Center"
+          }}
+        />
       </aside>
 
       <main className="flex-1 h-full overflow-y-auto p-4 rounded-tl-2xl">
